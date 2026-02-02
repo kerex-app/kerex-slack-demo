885b55f99b7797c7b57623f2a87ae49deb205f43
root <root@ip-10-0-1-67.eu-west-2.compute.internal>
2026-02-02 23:32:01 +0000

    feat: write a hello world function
    write a hello world function
    
    Co-Authored-By: Kerex Agent <agent@kerex.app>

---
+++ hello.ts
@@ -0,0 +1,12 @@
+/**
+ * A simple hello world function that logs to the console
+ */
+function helloWorld(): void {
+    console.log("Hello World!");
+}
+
+// Call the function
+helloWorld();
+
+// Export for potential reuse
+export default helloWorld;
