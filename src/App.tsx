7063f6a1a19e0afe2276bdd8c6f85241d7af9e3e
root <root@ip-10-0-1-39.eu-west-2.compute.internal>
2026-02-03 17:18:10 +0000

    feat: add subtitle to homepage (KER-5)

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,18 @@ function App() {
       height: '100vh',
       fontFamily: 'system-ui, sans-serif'
     }}>
-      <h1>Kerex Demo App</h1>
+      <div style={{ textAlign: 'center' }}>
+        <h1>Kerex Demo App</h1>
+        <p style={{
+          fontSize: '1.1rem',
+          color: '#666',
+          marginTop: '0.5rem',
+          fontWeight: '500',
+          letterSpacing: '0.5px'
+        }}>
+          Change has been made by Kerex agent!
+        </p>
+      </div>
     </div>
   )
 }
