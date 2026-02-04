afa8755c7d4e1bad2d945aece1ffe39bd63ceefb
root <root@ip-10-0-1-80.eu-west-2.compute.internal>
2026-02-04 00:33:22 +0000

    feat: add "Powered by Kerex" footer to homepage (KER-10)

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,24 @@ function App() {
   return (
     <div style={{
       display: 'flex',
+      flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'center',
-      height: '100vh',
+      minHeight: '100vh',
       fontFamily: 'system-ui, sans-serif'
     }}>
       <h1>Kerex Demo App</h1>
+      <footer style={{
+        marginTop: 'auto',
+        paddingTop: '40px',
+        paddingBottom: '20px',
+        color: '#888',
+        fontSize: '14px',
+        textAlign: 'center',
+        width: '100%'
+      }}>
+        Powered by Kerex
+      </footer>
     </div>
   )
 }
